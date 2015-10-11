d3cc8bde86aaa34029abb4b23977232cab533845
Alperen Elhan <alperen@elhan.org>
2015-10-12 01:28:28 +0300

    fix for signaller creation

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,35 +1,37 @@
-Meteor.startup(function () {
-  var mediaConfig = {
-    video: true,
-    audio: true
-  };
+Meteor.startup(function() {
+    var mediaConfig = {
+        video: true,
+        audio: true
+    };
 
-  var servers = {
-    iceServers: [{
-      url: "stun:stun.1.google.com:19302"
-    }]
-  };
+    var servers = {
+        iceServers: [{
+            url: "stun:stun.1.google.com:19302"
+        }]
+    };
 
-  var config = {};
+    var config = {};
 
-  webRTCSignaller = SingleWebRTCSignallerFactory.create(
-    'master',
-    servers,
-    config,
-    mediaConfig
-  );
+    webRTCSignaller = SingleWebRTCSignallerFactory.create(
+        Stream,
+        'test',
+        'master',
+        servers,
+        config,
+        mediaConfig
+    );
 
-  // Creates the rtcPeerConnection
-  // webRTCSignaller.start();
-  webRTCSignaller.startOnlyLocalStream();
+    // Creates the rtcPeerConnection
+    webRTCSignaller.start();
+    //webRTCSignaller.startOnlyLocalStream();
 });
 
 Template.layout.helpers({
-  localStream: function() {
-    return webRTCSignaller.getLocalStream();
-  },
+    localStream: function() {
+        return webRTCSignaller.getLocalStream();
+    },
 
-  remoteStream: function() {
-    return webRTCSignaller.getRemoteStream();
-  }
+    remoteStream: function() {
+        return webRTCSignaller.getRemoteStream();
+    }
 });
