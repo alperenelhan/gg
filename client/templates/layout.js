c376c2c6159b1dec8fd6edd7c62e9b023cf6be1b
Alperen Elhan <alperen@elhan.org>
2015-10-12 03:14:44 +0300

    added rooms

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -35,3 +35,27 @@ Template.layout.helpers({
         return webRTCSignaller.getRemoteStream();
     }
 });
+
+if (Meteor.isClient) {
+    Template.layout.events({
+        'click a.round': function (e) {
+            value = 3000;
+            var self = $(e.currentTarget);
+            self.css({
+                'transition' : 'all 2s ease-in-out'
+            });
+
+            self.bind("transitionend", function  (){
+                FlowRouter.go('/rooms');
+            });
+
+            self.css({
+              '-webkit-transform' : 'scale(' + value + ')',
+              '-moz-transform'    : 'scale(' + value + ')',
+              '-ms-transform'     : 'scale(' + value + ')',
+              '-o-transform'      : 'scale(' + value + ')',
+              'transform'         : 'scale(' + value + ')'
+            });
+        }
+    });
+}
